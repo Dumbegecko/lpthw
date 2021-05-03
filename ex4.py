8bef9459d14f20b99a81e45ad3e0e3afa12d5756
Dumbgecko <devgecko1@gmail.com>
2021-05-03 20:50:33 +0200

    ex4 add

---
+++ ex4.py
@@ -0,0 +1,18 @@
+my_name = 'Mike'
+my_age = 33 #not a lie
+my_height = 194 #centimeters
+my_weight = 120 #kilos
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'blond'
+
+print (f"let's talk about {my_name}.")
+print (f"He's {my_height} centimeters tall.")
+print (f"He's {my_weight} kilograms heavy.")
+print ("Actually thats not too heavy")
+print (f"He's got {my_eyes} eyes and {my_hair} hair.")
+print (f"His teeth are usually {my_teeth} depending on the quality of the coffee.")
+
+#this line is tricky to get right
+total = my_age + my_height + my_weight
+print(f"If i add {my_age},{my_height}, and {my_weight} I get {total}.")
